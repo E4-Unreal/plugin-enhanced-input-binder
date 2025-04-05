89f0489a93775f4a0cde2265342eb275976ca7c3
이정훈 <eu4ng97@gmail.com>
2025-04-05 18:47:59 +0900

    Chore: EnhancedInput 플러그인 및 모듈 종속성 추가

--- Source/EnhancedInputBinder/EnhancedInputBinder.Build.cs
+++ Source/EnhancedInputBinder/EnhancedInputBinder.Build.cs
@@ -20,7 +20,8 @@ public class EnhancedInputBinder : ModuleRules
                 "CoreUObject",
                 "Engine",
                 "Slate",
-                "SlateCore"
+                "SlateCore",
+                "EnhancedInput"
             }
         );
     }
