a8e564825d1a00e16c444347a31d8f1ecb510f99
이정훈 <eu4ng97@gmail.com>
2025-04-05 18:54:39 +0900

    Feat: InputMappingContextData 구조체 작성

---
+++ Source/EnhancedInputBinder/Public/Types/InputMappingContextData.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputMappingContextData.generated.h"
+
+class UInputMappingContext;
+
+USTRUCT(Atomic, BlueprintType)
+struct ENHANCEDINPUTBINDER_API FInputMappingContextData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UInputMappingContext> InputMappingContext;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 Priority;
+};
