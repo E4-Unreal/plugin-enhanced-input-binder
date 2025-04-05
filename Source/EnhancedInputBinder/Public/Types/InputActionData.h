68764d3b674dbf9c14a0f6f38f17052edf909c0e
이정훈 <eu4ng97@gmail.com>
2025-04-05 19:09:27 +0900

    Feat: InputActionData 구조체 작성

---
+++ Source/EnhancedInputBinder/Public/Types/InputActionData.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputTriggers.h"
+#include "InputActionData.generated.h"
+
+class UInputAction;
+
+USTRUCT(Atomic, BlueprintType)
+struct ENHANCEDINPUTBINDER_API FInputActionData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UInputAction> InputAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    ETriggerEvent TriggerEvent = ETriggerEvent::Triggered;
+};
