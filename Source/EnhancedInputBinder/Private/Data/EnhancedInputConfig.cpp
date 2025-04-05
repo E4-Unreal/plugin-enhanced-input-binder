6b63c83b91c99784530afc3b278697d76c25934c
이정훈 <eu4ng97@gmail.com>
2025-04-05 18:52:57 +0900

    Feat: EnhancedInputConfig 클래스 작성

---
+++ Source/EnhancedInputBinder/Private/Data/EnhancedInputConfig.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/EnhancedInputConfig.h"
+
+#include "EnhancedInputComponent.h"
+
+TArray<uint32> UEnhancedInputConfig::BindEnhancedInput(UEnhancedInputComponent* EnhancedInputComponent)
+{
+    return EnhancedInputComponent != nullptr ? OnBindEnhancedInput(EnhancedInputComponent) : TArray<uint32>();
+}
+
+APawn* UEnhancedInputConfig::GetOwningPlayerPawn(UEnhancedInputComponent* EnhancedInputComponent)
+{
+    if (EnhancedInputComponent)
+    {
+        if (AActor* OwningActor = EnhancedInputComponent->GetOwner())
+        {
+            if (APawn* OwningPawn = Cast<APawn>(OwningActor))
+            {
+                return OwningPawn;
+            }
+        }
+    }
+
+    return nullptr;
+}
+
+APlayerController* UEnhancedInputConfig::GetOwningPlayer(UEnhancedInputComponent* EnhancedInputComponent)
+{
+    if (APawn* OwningPawn = GetOwningPlayerPawn(EnhancedInputComponent))
+    {
+        return Cast<APlayerController>(OwningPawn->GetController());
+    }
+
+    return nullptr;
+}
