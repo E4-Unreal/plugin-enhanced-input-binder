dd3ec28ba408b411c516f7de6c0abafbfce7a30c
이정훈 <eu4ng97@gmail.com>
2025-04-05 18:58:30 +0900

    Feat: EnhancedInputBinderComponent 클래스 작성

---
+++ Source/EnhancedInputBinder/Private/Components/EnhancedInputBinderComponent.cpp
@@ -0,0 +1,150 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/EnhancedInputBinderComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Data/EnhancedInputConfig.h"
+#include "Logging.h"
+
+#define LOG_OWNER_ERROR LOG_ACTOR_COMPONENT(Error, TEXT("Owner should be PlayerController or Pawn!"))
+
+APawn* UEnhancedInputBinderComponent::GetOwningPawn() const
+{
+    if (IsPawn())
+    {
+        APawn* OwningPawn = Cast<APawn>(GetOwner());
+        return OwningPawn;
+    }
+    else if (IsPlayerController())
+    {
+        APlayerController* OwningPlayer = Cast<APlayerController>(GetOwner());
+        return OwningPlayer->GetPawn();
+    }
+    else
+    {
+        LOG_OWNER_ERROR
+
+        return nullptr;
+    }
+}
+
+APlayerController* UEnhancedInputBinderComponent::GetOwningPlayer() const
+{
+    if (IsPawn())
+    {
+        APawn* OwningPawn = Cast<APawn>(GetOwner());
+        return Cast<APlayerController>(OwningPawn->GetController());
+    }
+    else if (IsPlayerController())
+    {
+        APlayerController* OwningPlayer = Cast<APlayerController>(GetOwner());
+        return OwningPlayer;
+    }
+    else
+    {
+        LOG_OWNER_ERROR
+
+        return nullptr;
+    }
+}
+
+UEnhancedInputComponent* UEnhancedInputBinderComponent::GetEnhancedInputComponent() const
+{
+    if (IsPawn())
+    {
+        APawn* OwningPawn = Cast<APawn>(GetOwner());
+        return Cast<UEnhancedInputComponent>(OwningPawn->InputComponent);
+    }
+    else if (IsPlayerController())
+    {
+        APlayerController* OwningPlayer = Cast<APlayerController>(GetOwner());
+        return Cast<UEnhancedInputComponent>(OwningPlayer->InputComponent);
+    }
+    else
+    {
+        LOG_OWNER_ERROR
+
+        return nullptr;
+    }
+}
+
+UEnhancedInputLocalPlayerSubsystem* UEnhancedInputBinderComponent::GetEnhancedInputLocalPlayerSubsystem() const
+{
+    if (APlayerController* PlayerController = GetOwningPlayer())
+    {
+        return ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+    }
+
+    return nullptr;
+}
+
+void UEnhancedInputBinderComponent::BindEnhancedInput()
+{
+    BindInputConfigs();
+
+    AddMappingContexts();
+}
+
+void UEnhancedInputBinderComponent::UnBindEnhancedInput()
+{
+    UnBindInputConfigs();
+
+    RemoveMappingContexts();
+}
+
+void UEnhancedInputBinderComponent::BindInputConfigs()
+{
+    if (UEnhancedInputComponent* EnhancedInputComponent = GetEnhancedInputComponent())
+    {
+        for (const auto& InputConfig : InputConfigs)
+        {
+            if(InputConfig)
+            {
+                InputBindingHandles.Append(InputConfig->BindEnhancedInput(EnhancedInputComponent));
+            }
+        }
+    }
+}
+
+void UEnhancedInputBinderComponent::UnBindInputConfigs()
+{
+    if (UEnhancedInputComponent* EnhancedInputComponent = GetEnhancedInputComponent())
+    {
+        for (const auto& InputBindingHandle : InputBindingHandles)
+        {
+            EnhancedInputComponent->RemoveActionBindingForHandle(InputBindingHandle);
+        }
+
+        InputBindingHandles.Reset();
+    }
+}
+
+void UEnhancedInputBinderComponent::AddMappingContexts()
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        for (const auto& [InputMappingContext, Priority] : InputMappingContextDataList)
+        {
+            if (!Subsystem->HasMappingContext(InputMappingContext))
+            {
+                Subsystem->AddMappingContext(InputMappingContext, Priority);
+            }
+        }
+    }
+}
+
+void UEnhancedInputBinderComponent::RemoveMappingContexts()
+{
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = GetEnhancedInputLocalPlayerSubsystem())
+    {
+        for (const auto& [InputMappingContext, Priority] : InputMappingContextDataList)
+        {
+            if (Subsystem->HasMappingContext(InputMappingContext))
+            {
+                Subsystem->RemoveMappingContext(InputMappingContext);
+            }
+        }
+    }
+}
---
+++ Source/EnhancedInputBinder/Public/Components/EnhancedInputBinderComponent.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Types/InputMappingContextData.h"
+#include "EnhancedInputBinderComponent.generated.h"
+
+class UEnhancedInputConfig;
+class UEnhancedInputComponent;
+class UEnhancedInputLocalPlayerSubsystem;
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class ENHANCEDINPUTBINDER_API UEnhancedInputBinderComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Config")
+    TArray<FInputMappingContextData> InputMappingContextDataList;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Config")
+    TArray<TObjectPtr<UEnhancedInputConfig>> InputConfigs;
+
+    UPROPERTY(VisibleInstanceOnly, Category = "State")
+    TArray<uint32> InputBindingHandles;
+
+public:
+    UFUNCTION(BlueprintCallable)
+    virtual void BindEnhancedInput();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void UnBindEnhancedInput();
+
+protected:
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE bool IsPawn() const { return GetOwner()->GetClass()->IsChildOf(APawn::StaticClass()); }
+
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE bool IsPlayerController() const { return GetOwner()->GetClass()->IsChildOf(APlayerController::StaticClass()); }
+
+    UFUNCTION(BlueprintPure)
+    APawn* GetOwningPawn() const;
+
+    UFUNCTION(BlueprintPure)
+    APlayerController* GetOwningPlayer() const;
+
+    UFUNCTION(BlueprintPure)
+    UEnhancedInputComponent* GetEnhancedInputComponent() const;
+
+    UFUNCTION(BlueprintPure)
+    UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputLocalPlayerSubsystem() const;
+
+    UFUNCTION(BlueprintCallable)
+    virtual void BindInputConfigs();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void UnBindInputConfigs();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void AddMappingContexts();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void RemoveMappingContexts();
+};
