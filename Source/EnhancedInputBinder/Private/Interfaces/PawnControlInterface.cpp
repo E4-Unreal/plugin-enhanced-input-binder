be2b25445878a67482f921042fea3ec70298480c
이정훈 <eu4ng97@gmail.com>
2025-04-11 03:22:59 +0900

    Feat: PawnControlInterface 인터페이스 정의

---
+++ Source/EnhancedInputBinder/Private/Interfaces/PawnControlInterface.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/PawnControlInterface.h"
+
+#include "InputActionValue.h"
+#include "Logging.h"
+
+
+// Add default functionality here for any IPawnControlInterface functions that are not pure virtual.
+void IPawnControlInterface::Move_Implementation(FInputActionValue ActionValue, float ElapsedTime, float TriggeredTime,  const UInputAction* SourceAction)
+{
+    LOG_TODO_INTERFACE
+}
+
+void IPawnControlInterface::Look_Implementation(FInputActionValue ActionValue, float ElapsedTime, float TriggeredTime,  const UInputAction* SourceAction)
+{
+    LOG_TODO_INTERFACE
+}
---
+++ Source/EnhancedInputBinder/Public/Interfaces/PawnControlInterface.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PawnControlInterface.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+
+// This class does not need to be modified.
+UINTERFACE(Blueprintable)
+class ENHANCEDINPUTBINDER_API UPawnControlInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ *
+ */
+class ENHANCEDINPUTBINDER_API IPawnControlInterface
+{
+    GENERATED_BODY()
+
+    // Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void Move(FInputActionValue ActionValue, float ElapsedTime, float TriggeredTime, const UInputAction* SourceAction);
+    virtual void Move_Implementation(FInputActionValue ActionValue, float ElapsedTime, float TriggeredTime, const UInputAction* SourceAction);
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void Look(FInputActionValue ActionValue, float ElapsedTime, float TriggeredTime, const UInputAction* SourceAction);
+    virtual void Look_Implementation(FInputActionValue ActionValue, float ElapsedTime, float TriggeredTime, const UInputAction* SourceAction);
+};
