4c092270207a239687e92df14ae72d6f6dabf60a
이정훈 <eu4ng97@gmail.com>
2025-04-11 03:35:50 +0900

    Feat: InputConfig_PawnControl 클래스 작성

---
+++ Source/EnhancedInputBinder/Private/Input/InputConfig_PawnControl.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/InputConfig_PawnControl.h"
+
+#include "EnhancedInputComponent.h"
+#include "Interfaces/PawnControlInterface.h"
+
+TArray<uint32> UInputConfig_PawnControl::OnBindEnhancedInput(UEnhancedInputComponent* EnhancedInputComponent)
+{
+    TArray<uint32> InputBindingHandles;
+    auto OwningPawn = GetOwningPawn(EnhancedInputComponent);
+    if (OwningPawn == nullptr) return InputBindingHandles;
+    if (!OwningPawn->GetClass()->ImplementsInterface(UPawnControlInterface::StaticClass())) return InputBindingHandles;
+
+    // Bind Move Action
+    FEnhancedInputActionEventBinding& MoveActionBinding = EnhancedInputComponent->BindActionValueLambda(
+                MoveAction,
+                ETriggerEvent::Triggered,
+                [&](const FInputActionValue& InputActionValue)
+                {
+                    IPawnControlInterface::Execute_Move(OwningPawn, InputActionValue);
+                });
+
+    InputBindingHandles.Emplace(MoveActionBinding.GetHandle());
+
+    // Bind Look Action
+    FEnhancedInputActionEventBinding& LookActionBinding = EnhancedInputComponent->BindActionValueLambda(
+                LookAction,
+                ETriggerEvent::Triggered,
+                [&](const FInputActionValue& InputActionValue)
+                {
+                    IPawnControlInterface::Execute_Look(OwningPawn, InputActionValue);
+                });
+
+    InputBindingHandles.Emplace(LookActionBinding.GetHandle());
+
+    return InputBindingHandles;
+}
---
+++ Source/EnhancedInputBinder/Public/Input/InputConfig_PawnControl.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputConfig.h"
+#include "InputConfig_PawnControl.generated.h"
+
+class UInputAction;
+
+/**
+ *
+ */
+UCLASS(meta = (DisplayName = "InputConfig_PawnControl"))
+class ENHANCEDINPUTBINDER_API UInputConfig_PawnControl : public UInputConfig
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Config")
+    TObjectPtr<UInputAction> MoveAction;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Config")
+    TObjectPtr<UInputAction> LookAction;
+
+protected:
+    virtual TArray<uint32> OnBindEnhancedInput(UEnhancedInputComponent* EnhancedInputComponent) override;
+};
