1172e8cec1fc2a89d10bca2240074f8940341cc3
이정훈 <eu4ng97@gmail.com>
2025-04-22 04:01:04 +0900

    Refactor: InputBinderComponent

--- Source/EnhancedInputBinder/Public/Components/InputBinderComponent.h
+++ Source/EnhancedInputBinder/Public/Components/InputBinderComponent.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Types/InputBindingHandleList.h"
 #include "InputBinderComponent.generated.h"
 
-class UInputConfigSetting;
+class UEnhancedInputLocalPlayerSubsystem;
 class UInputMappingContext;
 class UInputConfigBase;
-class UEnhancedInputComponent;
-class UEnhancedInputLocalPlayerSubsystem;
+class UInputConfigSetting;
 
 UCLASS(meta = (BlueprintSpawnableComponent))
 class ENHANCEDINPUTBINDER_API UInputBinderComponent : public UActorComponent
@@ -18,49 +18,51 @@ class ENHANCEDINPUTBINDER_API UInputBinderComponent : public UActorComponent
     GENERATED_BODY()
 
     TWeakObjectPtr<UEnhancedInputComponent> EnhancedInputComponent;
+    TWeakObjectPtr<APlayerController> PlayerController;
+    TWeakObjectPtr<UEnhancedInputLocalPlayerSubsystem> EnhancedInputLocalPlayerSubsystem;
 
 protected:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Config")
-    int32 Priority;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Config")
     TSet<TObjectPtr<UInputConfigSetting>> InputConfigSettings;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Config")
-    TSet<TObjectPtr<UInputMappingContext>> InputMappingContexts;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Config")
-    TSet<TObjectPtr<UInputConfigBase>> InputConfigs;
-
     UPROPERTY(VisibleAnywhere, Transient, Category = "State")
-    TArray<uint32> InputBindingHandles;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "State")
-    bool bBound = false;
+    TMap<TObjectPtr<UInputConfigBase>, FInputBindingHandleList> InputBindingHandleMap;
 
 public:
     virtual void BeginPlay() override;
 
-    virtual void BindEnhancedInput();
-    virtual void UnBindEnhancedInput();
+    UFUNCTION(BlueprintCallable, Category = "InputBinderComponent")
+    virtual void AddInputMappingContext(UInputMappingContext* InputMappingContext, int32 Priority);
 
-    void SetEnhancedInputComponent(UEnhancedInputComponent* NewEnhancedInputComponent);
+    UFUNCTION(BlueprintCallable, Category = "InputBinderComponent")
+    virtual void RemoveInputMappingContext(UInputMappingContext* InputMappingContext);
+
+    UFUNCTION(BlueprintCallable, Category = "InputBinderComponent")
+    virtual void BindInputConfig(UInputConfigBase* InputConfig);
+
+    UFUNCTION(BlueprintCallable, Category = "InputBinderComponent")
+    virtual void UnBindInputConfig(UInputConfigBase* InputConfig);
+
+    UFUNCTION(BlueprintCallable, Category = "InputBinderComponent")
+    virtual void BindInputConfigSetting(UInputConfigSetting* InputConfigSetting);
+
+    UFUNCTION(BlueprintCallable, Category = "InputBinderComponent")
+    virtual void UnBindInputConfigSetting(UInputConfigSetting* InputConfigSetting);
 
 protected:
     virtual void FindEnhancedInputComponent();
 
-    virtual void AddMappingContexts();
-    virtual void RemoveMappingContexts();
-
-    virtual void BindInputConfigs();
-    virtual void UnBindInputConfigs();
-
-    FORCEINLINE UEnhancedInputComponent* GetEnhancedInputComponent() const { return EnhancedInputComponent.Get(); }
-
-    APawn* GetOwningPawn() const;
-    APlayerController* GetOwningPlayerController() const;
-    UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputLocalPlayerSubsystem() const;
+    virtual void BindEnhancedInput();
+    virtual void UnBindEnhancedInput();
 
     UFUNCTION()
     virtual void OnOwningPawnRestarted(APawn* OwningPawn);
+
+public:
+    void SetEnhancedInputComponent(UEnhancedInputComponent* NewEnhancedInputComponent);
+
+protected:
+    FORCEINLINE UEnhancedInputComponent* GetEnhancedInputComponent() const { return EnhancedInputComponent.Get(); }
+    FORCEINLINE APlayerController* GetPlayerController() const { return PlayerController.Get(); }
+    FORCEINLINE UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputLocalPlayerSubsystem() const { return EnhancedInputLocalPlayerSubsystem.Get(); }
 };
---
+++ Source/EnhancedInputBinder/Public/Types/InputBindingHandleList.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputBindingHandleList.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FInputBindingHandleList
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<int32> Handles;
+
+    FInputBindingHandleList() {}
+
+    FInputBindingHandleList(const TArray<uint32>& InHandles)
+    {
+        Handles.Empty(InHandles.Num());
+        for (uint32 Handle : InHandles)
+        {
+            Handles.Emplace(Handle);
+        }
+    }
+};
