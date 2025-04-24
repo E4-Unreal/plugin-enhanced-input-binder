4cc1f5e41b3675bbd4405346405736eee83dea94
이정훈 <eu4ng97@gmail.com>
2025-04-25 08:58:54 +0900

    Feat: InputBinderPlayerController 클래스 작성

---
+++ Source/EnhancedInputBinder/Private/PlayerControllers/InputBinderPlayerController.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerControllers/InputBinderPlayerController.h"
+
+#include "Components/InputBinderComponent.h"
+
+const FName AInputBinderPlayerController::InputBinderName(TEXT("InputBinder"));
+
+AInputBinderPlayerController::AInputBinderPlayerController(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    InputBinder = CreateDefaultSubobject<UInputBinderComponent>(InputBinderName);
+}
---
+++ Source/EnhancedInputBinder/Public/PlayerControllers/InputBinderPlayerController.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InputBinderPlayerController.generated.h"
+
+class UInputBinderComponent;
+/**
+ *
+ */
+UCLASS()
+class ENHANCEDINPUTBINDER_API AInputBinderPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+protected:
+    static const FName InputBinderName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputBinderComponent> InputBinder;
+
+public:
+    AInputBinderPlayerController(const FObjectInitializer& ObjectInitializer);
+
+public:
+    FORCEINLINE UInputBinderComponent* GetInputBinder() const { return InputBinder; }
+};
