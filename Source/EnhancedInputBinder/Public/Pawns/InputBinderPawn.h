01cbbdce7ccad67063266d2ad130a46daacc088a
이정훈 <eu4ng97@gmail.com>
2025-04-25 09:03:27 +0900

    Feat: InputBinderPawn 클래스 작성

---
+++ Source/EnhancedInputBinder/Public/Pawns/InputBinderPawn.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "InputBinderPawn.generated.h"
+
+class UInputBinderComponent;
+
+UCLASS()
+class ENHANCEDINPUTBINDER_API AInputBinderPawn : public APawn
+{
+    GENERATED_BODY()
+
+protected:
+    static const FName InputBinderName;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputBinderComponent> InputBinder;
+
+public:
+    AInputBinderPawn(const FObjectInitializer& ObjectInitializer);
+
+public:
+    FORCEINLINE UInputBinderComponent* GetInputBinder() const { return InputBinder; }
+};
